6c0988639f41ae64f0e2250ee9241aa438d59c51
Sujal Raj <sujalraj2064@gmail.com>
2026-04-25 17:41:15 +0530

    update CORS configuration to allow requests from localhost

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://nexus-ruby-eta.vercel.app",
+            value: "http://localhost:3000/",
           },
           {
             key: "Access-Control-Allow-Methods",
